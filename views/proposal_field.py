0a7064f05794f9aab7482930d0acf5633b6c4103
Alice Lima <leriasler@gmail.com>
2023-06-23 14:28:30 -0300

    proposal field view

--- views/proposal_field.py
+++ views/proposal_field.py
@@ -16,7 +16,7 @@ def create_proposal_field():
     try:
         proposal_field_data = serializer.load(data)
     except ValidationError as error:
-        return jsonify({'errors': error.messages}, 400)
+        return jsonify({'errors': error.messages}), 400
     proposal_field = ProposalField(**proposal_field_data)
 
     db.session.add(proposal_field)
@@ -33,3 +33,30 @@ def get_proposal_fields():
     response = bs.dump(proposal_fields)
     return jsonify(response), 200
 
+
+@proposal_field_bp.route('/proposal_fields/<field_name>', methods=["POST"])
+def update_proposal_field(field_name):
+    proposal_field = ProposalField.query.filter_by(name=field_name).first()
+    if not proposal_field:
+        return jsonify({'error': 'Proposal field not found'}), 404
+    proposal_field_data = request.json
+    for key, value in proposal_field_data.items():
+        setattr(proposal_field, key, value)
+    db.session.commit()
+    response = serializer.dump(proposal_field)
+    return jsonify(response), 200
+
+@proposal_field_bp.route('/proposal_fields/<field_name>', methods=["DELETE"])
+def delete_proposal_field(field_name):
+    proposal_field = ProposalField.query.filter_by(name=field_name).first()
+    if not proposal_field:
+        return jsonify({'error': 'Proposal field not found'}), 404
+    db.session.delete(proposal_field)
+    db.session.commit()
+    return jsonify({"message": "proposal field deleted sucessfully"}), 200
+
+    
+
+
+    
+       
