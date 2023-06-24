539aeb46d48fb9debb6cd71a3c3dad425c60db6c
Alice Lima <leriasler@gmail.com>
2023-06-24 13:57:37 -0300

    frontend

---
+++ frontend/src/api/mutations.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+import configs from "./../configs/configs";
+import { Responses } from "../types";
+
+export const createResponse = async (input: Responses) => {
+  const url = new URL(configs.API_URL + "/proposal");
+  await axios.post(url.toString(), input);
+};
---
+++ frontend/src/types.ts
@@ -0,0 +1,19 @@
+export type Response = {
+  field_name: string;
+  value: string | number;
+};
+
+export type Responses = ResultList<"responses", Response>;
+
+export type ProposalField = {
+  name: string;
+  type: any;
+  nullable: boolean;
+  order: number;
+};
+
+export type ProposalFields = ProposalField[];
+
+export type ResultList<K extends string, T> = {
+  [P in K]: T[];
+};
